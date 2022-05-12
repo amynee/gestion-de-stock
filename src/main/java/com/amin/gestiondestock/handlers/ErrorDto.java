f340a72b18500e22a77216798d8338cca2a31573
amyyne <tlijaniamine9@gmail.com>
2022-05-12 02:12:33 +0100

    DAO, and global handler for Exceptions

---
+++ src/main/java/com/amin/gestiondestock/handlers/ErrorDto.java
@@ -0,0 +1,26 @@
+package com.amin.gestiondestock.handlers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.amin.gestiondestock.exception.ErrorCodes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+	private Integer httpCode;
+	private ErrorCodes code;
+	private String message;
+	private List<String> errors = new ArrayList<>();
+	
+}
---
+++ src/main/java/com/amin/gestiondestock/handlers/RestExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.amin.gestiondestock.handlers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.amin.gestiondestock.exception.EntityNotFoundException;
+import com.amin.gestiondestock.exception.InvalidEntityException;
+
+@RestControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorDto> handleExcception(EntityNotFoundException exception, WebRequest webRequest) {
+		
+		final HttpStatus notFound = HttpStatus.NOT_FOUND;
+		final ErrorDto errorDto = ErrorDto.builder()
+				.code(exception.getErrorCode())
+				.httpCode(notFound.value())
+				.message(exception.getMessage())
+				.build();
+		
+		return new ResponseEntity<>(errorDto, notFound);
+	}
+	
+	@ExceptionHandler(InvalidEntityException.class)
+	public ResponseEntity<ErrorDto> handleExcception(InvalidEntityException exception, WebRequest webRequest) {
+		
+		final HttpStatus badResuest = HttpStatus.BAD_REQUEST;
+		final ErrorDto errorDto = ErrorDto.builder()
+				.code(exception.getErrorCode())
+				.httpCode(badResuest.value())
+				.message(exception.getMessage())
+				.errors(exception.getErrors())
+				.build();
+		
+		return new ResponseEntity<>(errorDto, badResuest);
+	}
+}
